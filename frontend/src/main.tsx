17a3e0021661ac50f9f12c929f33d3562941d7bc
Yuval Khoramian <yuval@Yuvals-MacBook-Pro.local>
2026-06-08 10:43:53 +0300

    fonts are now in project and not imported from web

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+import '@fontsource/plus-jakarta-sans/300.css';
+import '@fontsource/plus-jakarta-sans/400.css';
+import '@fontsource/plus-jakarta-sans/500.css';
+import '@fontsource/plus-jakarta-sans/600.css';
+import '@fontsource/plus-jakarta-sans/700.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
